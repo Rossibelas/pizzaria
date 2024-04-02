a28cc13440e9b971b0e19438ad1cbdfd646eba5d
Rossibelas <guilhermerossi103@gmail.com>
2024-04-01 22:20:22 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php include("cabecalho.php") ?>
+<?php include("menu.php") ?>
+
+        <main class="bg-light row pe-3 ps-3">
+            <div class="row">
+                <h2 class="mt-4 h1">A melhor pizza da região</h2>
+                <hr>
+                <div class="col text-center">
+                    <img src="calabreza.png" alt="Calabresa">
+                    <p>Calabresa</p>
+                </div>
+
+                <div class="col text-center">
+                    <img src="calabreza.png" alt="Calabresa">
+                    <p>Calabresa</p>
+                </div>
+
+                <div class="col text-center">
+                    <img src="calabreza.png" alt="Calabresa">
+                    <p>Calabresa</p>
+                </div>
+                <hr>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <h3>Nossos sabores</h3>
+                    <p class="sabores"> À Moda da Casa  <br>
+                        Aliche <br>
+                        Atum <br>
+                        Baiana <br>
+                        Bauru <br>
+                        Canadense <br>
+                        Catupiry <br>
+                        Calabresa <br>
+                        À Moda da Casa  <br>
+                        Aliche <br>
+                        Atum <br>
+                        Baiana <br>
+                        Bauru <br>
+                        Canadense <br>
+                        Catupiry <br>
+                        Calabresa
+                    </p>
+                </div><!-- coluna -->
+                <div class="col">
+                    <h3>Uma casa com história</h3>
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="historia.png" class="img-fluid" alt="historia">
+                        </div>
+                        <div class="col">
+                            <p>Muito famosa em toda a região por sua excelência na qualidade, a nossa pizzzaria tem mais de 25 anos de história.</p>
+                            <p>
+                                <a href="quem-somos.php" class="link-destaque">Conheça</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- linha -->
+        </main>
+       
+        <?php include("rodape.php") ?>
+        
+        <footer class="row rodape ps-3 pe-3 pt-3">
+            <div class="col">
+                <p class="text-light fw-lighter">Aula do Senac. Declaração dos direitos autorais.</p>
+            </div>
+        </footer>
+    </div>
+    
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
